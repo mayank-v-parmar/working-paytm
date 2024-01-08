2dccd8077356b3335ba5f32db8be575389303010
mayank-v-parmar <mvp16798@gmail.com>
2024-01-08 14:39:26 +0530

    Adding code of paytm clone website

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors :{
+        paytmblue: "#2abaf2",
+        paytmDarkBlue: "#032970",
+        hoverBlue: "#27afe3",
+        hoverDarkBlue: "#0d3e80",
+        bgcolor: "#f5f7fa",
+        bullCard:"#00baf2",
+        footerBg:"#f5f7fa",
+        ftextColor:"#505050",
+      },
+    },
+  },
+  plugins: [],
+}
+
